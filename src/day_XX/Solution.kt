d37e7efbd931c41834ba8cf4858d65ed5025f072
Mohamed <algiriany93@gmail.com>
2022-12-06 10:26:16 +0530

    Update template

--- src/day_XX/Solution.kt
+++ src/day_XX/Solution.kt
@@ -8,21 +8,21 @@ private const val DAY = "XX"
 fun main() {
     fun part1(input: List<String>): Int {
 
-        return input.size
+        return 0
     }
 
     fun part2(input: List<String>): Int {
 
-        return input.size
+        return 0
     }
 
     // Test
     val testInput = readInput(DAY, testInput = true)
     check(part1(testInput) == 0) { "Part 1 failed!" }
-    //check(part2(testInput) == 0) { "Part 2 failed!" }
+    check(part2(testInput) == 0) { "Part 2 failed!" }
 
     // Result
     val input = readInput(DAY)
     println(part1(input))
-    //println(part2(input))
+    println(part2(input))
 }
